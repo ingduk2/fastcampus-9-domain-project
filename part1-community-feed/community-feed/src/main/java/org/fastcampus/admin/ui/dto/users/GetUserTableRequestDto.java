d9f423b1a7effd3a6a79230a5b3ef2184dc4b26f
ingduk2 <ingduk2@naver.com>
2024-11-19 20:16:34 +0900

    [9개 도메인 프로젝트로 끝내는 백엔드 웹 개발 (Java/Spring)] Part2. Spring JPA 를 활용한 커뮤니티 피드 서비스 고도화 - Ch 3. Mysql 쿼리 튜닝을 활용한 어드민 기능 구현 -- 04. 회원 관리 페이지 구현하기 -- GetUserTableRequestDto 페이징 부분 수정

--- part1-community-feed/community-feed/src/main/java/org/fastcampus/admin/ui/dto/users/GetUserTableRequestDto.java
+++ part1-community-feed/community-feed/src/main/java/org/fastcampus/admin/ui/dto/users/GetUserTableRequestDto.java
@@ -1,15 +1,34 @@
 package org.fastcampus.admin.ui.dto.users;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.fastcampus.common.domain.Pageable;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class GetUserTableRequestDto extends Pageable {
+public class GetUserTableRequestDto {
 
     private String name;
+    private Pageable pageable;
+
+    public GetUserTableRequestDto(String name, Integer pageIndex, Integer pageSize) {
+        this.name = name;
+        this.pageable = new Pageable(
+                pageIndex != null ? pageIndex : 1,
+                pageSize != null ? pageSize : 10
+        );
+    }
+
+    public int getOffset() {
+        return pageable.getOffset();
+    }
+
+    public int getLimit() {
+        return pageable.getLimit();
+    }
+
+    public int getPageIndex() {
+        return pageable.getPageIndex();
+    }
 }
